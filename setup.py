af0b2b4ae205adb59e9e1922ca6ec10d491ebf2a
Antoine Albertelli <antoinea101@gmail.com>
2015-02-05 13:15:24 +0100

    Use BeautifulSoup 4.x instead of 3.x
    BS 3.x is obsolete, switch to more recent version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='tequila-sessions',
-      version='1.0.0',
+      version='1.0.1',
       description='Requests session for Tequila (EPFL login manager)',
       author='Antoine Albertelli',
       author_email='antoine.albertelli+github@gmail.com',
       url='https://github.com/antoinealb/python-tequila',
       py_modules=['tequila'],
-      install_requires=['requests', 'BeautifulSoup'],
+      install_requires=['requests', 'beautifulsoup4'],
       )
